5420760d45875208caec6c4099f480a5f21386f2
balaba <keyend@163.com>
2023-02-07 15:08:17 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
-#############################################################################
-#
-#                    GNU LESSER GENERAL PUBLIC LICENSE
-#                        Version 3, 29 June 2007
-#
-#  Copyright (C) [2007] [TRON Foundation], Inc. <https://fsf.org/>
-#  Everyone is permitted to copy and distribute verbatim copies
-#  of this license document, but changing it is not allowed.
-#
-#
-#   This version of the GNU Lesser General Public License incorporates
-# the terms and conditions of version 3 of the GNU General Public
-# License, supplemented by the additional permissions listed below.
-#
-# You can find java-tron at https://github.com/quaki/java-ts/
-#
-##############################################################################
 
 BASE_DIR="/java-ts"
 DOCKER_REPOSITORY="quaki"
